cc01015b1ef0d780b649c058ba27bbbc8581b709
crittoo96 <crittoo96@gmail.com>
2023-07-18 16:19:56 +0900

    add edate validator

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,32 @@
 var express = require("express");
 var router = express.Router();
 const { validationResult } = require("express-validator");
-const { validateCardNumber } = require("./validator");
+const { validateCardNumber, validateEndDate } = require("./validator");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.json({ title: "Welcome to credit card validator" });
 });
 
-router.post("/credit/verify", [validateCardNumber], (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.json({ errors: errors, success: false });
+router.post(
+  "/credit/verify",
+  [validateCardNumber, validateEndDate],
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.json({ errors: errors, success: false });
+    }
+
+    const { cname, cno, edate } = req.body;
+
+    res.json({
+      success: true,
+      message: "Credit card verified successfully!",
+      cname,
+      cno,
+      edate,
+    });
   }
-
-  const { cname, cno, edate } = req.body;
-
-  res.json({
-    success: true,
-    message: "Credit card verified successfully!",
-    cname,
-    cno,
-    edate,
-  });
-});
+);
 
 module.exports = router;
--- routes/validator.js
+++ routes/validator.js
@@ -1,4 +1,6 @@
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
+const moment = require("moment");
+
 module.exports = {
   validateCardNumber: check("cno")
     // To delete leading and trailing space
@@ -10,4 +12,18 @@ module.exports = {
 
     // Custom message
     .withMessage("Must be a valid credit card number"),
+
+  validateEndDate: body("edate")
+    .custom((value, { req }) => {
+      if (!moment(value, "YYYY-MM", true).isValid()) {
+        throw new Error("Invalid date. Format should be YYYY-MM");
+      }
+      return true;
+    })
+    .custom((value, { req }) => {
+      if (moment(value).isSameOrBefore(moment())) {
+        throw new Error("Date should be in the future.");
+      }
+      return true;
+    }),
 };
